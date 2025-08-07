f724a21ec1c4ba49b3dfd90d62988c6340b5a51b
zakaahmed1 <zakaahmed1@hotmail.com>
2025-08-07 15:14:43 +0100

    Update streamlit_app.py
    corrected typo on line 50

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefoot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
